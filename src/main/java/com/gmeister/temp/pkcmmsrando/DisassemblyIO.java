417f13fcf25ad1ba611bcda54d614f9f8e53f98f
The G-Meister <grantfutcher@live.co.uk>
2021-04-04 22:44:53 +0100

    Remove redundant code
    Remove the Route 29 generator
    Remove a second method of importing tilesets

--- src/main/java/com/gmeister/temp/pkcmmsrando/DisassemblyIO.java
+++ src/main/java/com/gmeister/temp/pkcmmsrando/DisassemblyIO.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -415,35 +414,6 @@ public class DisassemblyIO
 		File outputFolder = this.outputFolder.toPath().resolve(path).toFile();
 		if (!outputFolder.exists()) outputFolder.mkdirs();
 		return outputFolder;
-	}
-	
-	private TileSet importTileSet(String name, File metadata) throws FileNotFoundException, IOException
-	{
-		//check if files exist yada yada yada
-		
-		TileSet output = new TileSet(name);
-		Pattern tab = Pattern.compile("\t");
-		
-		try (BufferedReader reader = new BufferedReader(new FileReader(metadata)))
-		{
-			if (!reader.ready()) throw new IOException("File contains no data");
-			ArrayList<String> headers = new ArrayList<>(Arrays.asList(tab.split(reader.readLine())));
-			
-			while (reader.ready())
-			{
-				Tile tile = new Tile();
-				String[] tileData = tab.split(reader.readLine());
-				if (tileData.length < headers.size()) throw new IOException("Not all lines have the same length");
-				
-				tile.setName(tileData[headers.indexOf("Name")]);
-				tile.setBuilding(!tileData[headers.indexOf("Building")].equals("0"));
-				int id = Integer.parseInt(tileData[headers.indexOf("Tile ID Decimal")]);
-				while (id < output.getTiles().size() - 1) output.getTiles().add(null);
-				output.getTiles().set(id, tile);
-			}
-		}
-		
-		return output;
 	}
 	
 	/**
