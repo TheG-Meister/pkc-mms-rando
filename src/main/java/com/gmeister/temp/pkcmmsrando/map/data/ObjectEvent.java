2250c5f66f4c3bd779341303c282087eece5c469
The G-Meister <grantfutcher@live.co.uk>
2022-02-01 14:58:37 +0000

    Create ObjectEvent

---
+++ src/main/java/com/gmeister/temp/pkcmmsrando/map/data/ObjectEvent.java
@@ -0,0 +1,28 @@
+package com.gmeister.temp.pkcmmsrando.map.data;
+
+public class ObjectEvent
+{
+	
+	private OverworldPosition position;
+	//This sprite will become invisible and intangible when this flag is set
+	private Flag flag;
+	
+	public ObjectEvent(OverworldPosition position, Flag flag)
+	{
+		this.position = position;
+		this.flag = flag;
+	}
+
+	public OverworldPosition getPosition()
+	{ return this.position; }
+
+	public void setPosition(OverworldPosition position)
+	{ this.position = position; }
+
+	public Flag getFlag()
+	{ return this.flag; }
+
+	public void setFlag(Flag flag)
+	{ this.flag = flag; }
+	
+}
