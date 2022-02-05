ab846fe32f9a3d7ac1feafc7c437325cc49bf33c
The G-Meister <grantfutcher@live.co.uk>
2022-02-05 20:00:30 +0000

    Add a CollisionConstant to CoordEvent

--- src/main/java/com/gmeister/temp/pkcmmsrando/map/data/CoordEvent.java
+++ src/main/java/com/gmeister/temp/pkcmmsrando/map/data/CoordEvent.java
@@ -4,14 +4,24 @@ public class CoordEvent
 {
 	
 	private OverworldPosition position;
+	private CollisionConstant simulatedCollision;
 	
-	public CoordEvent(OverworldPosition position)
-	{ this.position = position; }
-	
+	public CoordEvent(OverworldPosition position, CollisionConstant simulatedCollision)
+	{
+		this.position = position;
+		this.simulatedCollision = simulatedCollision;
+	}
+
 	public OverworldPosition getPosition()
 	{ return this.position; }
 	
 	public void setPosition(OverworldPosition position)
 	{ this.position = position; }
+
+	public CollisionConstant getSimulatedCollision()
+	{ return this.simulatedCollision; }
+
+	public void setSimulatedCollision(CollisionConstant simulatedCollision)
+	{ this.simulatedCollision = simulatedCollision; }
 	
 }
