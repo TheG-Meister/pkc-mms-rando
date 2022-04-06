2fbdef4767c8dbcc94f04d4543d6e4285b62cf76
The G-Meister <grantfutcher@live.co.uk>
2022-04-06 13:11:16 +0100

    Add printEdgeTable, cloning constructor and components to Network

--- src/main/java/com/gmeister/temp/pkcmmsrando/network/Network.java
+++ src/main/java/com/gmeister/temp/pkcmmsrando/network/Network.java
@@ -10,6 +10,7 @@ public class Network<N, B extends Branch<N>>
 	
 	private Set<N> nodes;
 	private Map<N, Set<B>> branchMap;
+	private Set<Set<N>> components;
 	
 	public Network(Set<N> nodes, Set<B> branches)
 	{
@@ -18,6 +19,18 @@ public class Network<N, B extends Branch<N>>
 		
 		for (N node : nodes) this.addNode(node);
 		for (B branch : branches) this.addBranch(branch);
+		
+		this.setUpComponents();
+	}
+	
+	public Network(Network<N, B> other)
+	{
+		this.nodes = new HashSet<>(other.nodes);
+		
+		this.branchMap = new HashMap<>();
+		for (Set<? extends B> branches : other.branchMap.values()) for (B branch : branches) this.addBranch(branch);
+		
+		this.setUpComponents();
 	}
 	
 	public Set<N> getNodes()
@@ -52,5 +65,48 @@ public class Network<N, B extends Branch<N>>
 	public void addBranch(B branch)
 	{ this.getBranchEntry(branch.getSource())
 			.add(branch); }
+	
+	public void printEdgeTable()
+	{
+		System.out.println("Source\tTarget");
+		for (N key : this.branchMap.keySet()) for (B branch : this.branchMap.get(key))
+			System.out.println(
+					branch.getSource() + "\t" +
+					branch.getTarget());
+	}
+	
+	private void setUpComponents()
+	{
+		this.components = new HashSet<>();
+		Map<N, Set<N>> componentMap = new HashMap<>();
+		
+		for (N node : this.nodes)
+		{
+			Set<N> set = new HashSet<>();
+			set.add(node);
+			this.components.add(set);
+			componentMap.put(node, set);
+		}
+		
+		for (N source : this.branchMap.keySet())
+		{
+			Set<N> sourceComponent = componentMap.get(source);
+			for (B edge : this.getBranchEntry(source)) if (!sourceComponent.contains(edge.getTarget()))
+			{
+				Set<N> targetComponent = componentMap.get(edge.getTarget());
+				sourceComponent.addAll(targetComponent);
+				this.components.remove(targetComponent);
+				for (N node : targetComponent) componentMap.put(node, sourceComponent);
+			}
+		}
+	}
+	
+	public Set<N> getComponentOf(N node)
+	{
+		if (node == null) throw new IllegalArgumentException("node cannot be null");
+		if (!this.nodes.contains(node)) throw new IllegalArgumentException("node is not part of the network");
+		
+		return this.components.stream().filter(c -> c.contains(node)).findAny().orElseThrow();
+	}
 	
 }
