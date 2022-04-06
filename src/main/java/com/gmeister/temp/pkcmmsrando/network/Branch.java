1fb01e37c4fd8924893b4181723a5b22637d853a
The G-Meister <grantfutcher@live.co.uk>
2022-04-06 13:10:27 +0100

    Add toString, hashCode and equals to Branch

--- src/main/java/com/gmeister/temp/pkcmmsrando/network/Branch.java
+++ src/main/java/com/gmeister/temp/pkcmmsrando/network/Branch.java
@@ -18,5 +18,39 @@ public class Branch<N>
 	
 	public N getTarget()
 	{ return this.target; }
+
+	@Override
+	public String toString()
+	{ return "Branch [source=" + this.source + ", target=" + this.target + "]"; }
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.source == null) ? 0 : this.source.hashCode());
+		result = prime * result + ((this.target == null) ? 0 : this.target.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Branch<?> other = (Branch<?>) obj;
+		if (this.source == null)
+		{
+			if (other.source != null) return false;
+		}
+		else if (!this.source.equals(other.source)) return false;
+		if (this.target == null)
+		{
+			if (other.target != null) return false;
+		}
+		else if (!this.target.equals(other.target)) return false;
+		return true;
+	}
 	
 }
