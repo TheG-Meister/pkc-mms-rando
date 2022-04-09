3422cad4592d8f2b7a4be7ba1d8134ba034a5177
The G-Meister <grantfutcher@live.co.uk>
2022-04-09 16:27:30 +0100

    Add components to Network

--- src/main/java/com/gmeister/temp/pkcmmsrando/network/Network.java
+++ src/main/java/com/gmeister/temp/pkcmmsrando/network/Network.java
@@ -1,5 +1,6 @@
 package com.gmeister.temp.pkcmmsrando.network;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,34 +13,43 @@ public class Network<N, B extends Branch<N>>
 	private Map<N, Set<B>> branchMap;
 	private Set<Set<N>> components;
 	
-	public Network(Set<N> nodes, Set<B> branches)
+	public Network(Collection<? extends N> nodes, Collection<? extends B> branches)
 	{
 		this.nodes = new HashSet<>();
 		this.branchMap = new HashMap<>();
+		this.components = new HashSet<>();
 		
-		for (N node : nodes) this.addNode(node);
-		for (B branch : branches) this.addBranch(branch);
-		
-		this.setUpComponents();
+		this.addNodes(nodes);
+		this.addBranches(branches);
 	}
 	
-	public Network(Network<N, B> other)
+	public Network(Network<? extends N, ? extends B> other)
 	{
-		this.nodes = new HashSet<>(other.nodes);
-		
+		this.nodes = new HashSet<>();
 		this.branchMap = new HashMap<>();
-		for (Set<? extends B> branches : other.branchMap.values()) for (B branch : branches) this.addBranch(branch);
+		this.components = new HashSet<>();
 		
-		this.setUpComponents();
+		this.addNodes(other.getNodes());
+		
+		Set<B> branches = new HashSet<>();
+		for (Set<? extends B> nodeBranches : other.branchMap.values()) branches.addAll(nodeBranches);
+		this.addBranches(branches);
 	}
 	
 	public Set<N> getNodes()
 	{ return new HashSet<>(this.nodes); }
 	
+	public Set<Set<N>> getComponents()
+	{
+		Set<Set<N>> components = new HashSet<>();
+		for (Set<N> component : this.components) components.add(new HashSet<>(component));
+		return components;
+	}
+	
 	private Set<B> getBranchEntry(N source)
 	{
-		if (source == null) throw new IllegalArgumentException("source node cannot be null");
-		if (!this.nodes.contains(source)) throw new IllegalArgumentException("This node is not part of the network");
+		if (source == null) throw new IllegalArgumentException("source node must not be null");
+		if (!this.nodes.contains(source)) throw new IllegalArgumentException("source node must be part of the network");
 		
 		Set<B> branches = this.branchMap.get(source);
 		//This should never be the case as addNode also adds entries to branchMap
@@ -54,17 +64,59 @@ public class Network<N, B extends Branch<N>>
 	public Set<B> getBranches(N source)
 	{ return new HashSet<>(this.getBranchEntry(source)); }
 	
+	public void addNodes(Collection<? extends N> nodes)
+	{
+		if (nodes == null) throw new IllegalArgumentException("nodes must not be null");
+		if (nodes.contains(null)) throw new IllegalArgumentException("nodes must not contain null elements");
+		
+		for (N node : nodes) this.addNode(node);
+	}
+	
 	public void addNode(N node)
 	{
-		if (node == null) throw new IllegalArgumentException("node cannot be null");
+		if (node == null) throw new IllegalArgumentException("node must not be null");
 		
 		this.nodes.add(node);
 		this.branchMap.put(node, new HashSet<>());
+		
+		Set<N> component = new HashSet<>();
+		component.add(node);
+		this.components.add(component);
+	}
+	
+	protected void validateBranch(B branch)
+	{
+		if (branch == null) throw new IllegalArgumentException("branch must not be null");
+		
+		if (branch.getSource() == null) throw new IllegalArgumentException("branch source must not be null");
+		if (branch.getTarget() == null) throw new IllegalArgumentException("branch target must not be null");
+		
+		if (!this.nodes.contains(branch.getSource())) throw new IllegalArgumentException("branch source must be part of the network");
+		if (!this.nodes.contains(branch.getTarget())) throw new IllegalArgumentException("branch target must be part of the network");
+	}
+	
+	public void addBranches(Collection<? extends B> branches)
+	{
+		if (branches == null) throw new IllegalArgumentException("branches must not be null");
+		
+		//Validate all branches
+		for (B branch : branches) this.validateBranch(branch);
+		
+		//Add all branches
+		for (B branch : branches) this.addBranch(branch);
 	}
 	
 	public void addBranch(B branch)
-	{ this.getBranchEntry(branch.getSource())
-			.add(branch); }
+	{
+		this.validateBranch(branch);
+		
+		this.getBranchEntry(branch.getSource())
+			.add(branch);
+		
+		Set<N> sourceComponent = this.getComponentOf(branch.getSource());
+		Set<N> targetComponent = this.getComponentOf(branch.getTarget());
+		if (sourceComponent != targetComponent) this.mergeComponents(sourceComponent, targetComponent);
+	}
 	
 	public void printEdgeTable()
 	{
@@ -75,17 +127,20 @@ public class Network<N, B extends Branch<N>>
 					branch.getTarget());
 	}
 	
-	private void setUpComponents()
+	/**
+	 * Initialises the set of components. This can be used to recalculate the components of this network if any of the nodes or branches have been modified.
+	 */
+	public void initComponents()
 	{
 		this.components = new HashSet<>();
 		Map<N, Set<N>> componentMap = new HashMap<>();
 		
 		for (N node : this.nodes)
 		{
-			Set<N> set = new HashSet<>();
-			set.add(node);
-			this.components.add(set);
-			componentMap.put(node, set);
+			Set<N> component = new HashSet<>();
+			component.add(node);
+			this.components.add(component);
+			componentMap.put(node, component);
 		}
 		
 		for (N source : this.branchMap.keySet())
@@ -94,17 +149,33 @@ public class Network<N, B extends Branch<N>>
 			for (B edge : this.getBranchEntry(source)) if (!sourceComponent.contains(edge.getTarget()))
 			{
 				Set<N> targetComponent = componentMap.get(edge.getTarget());
-				sourceComponent.addAll(targetComponent);
-				this.components.remove(targetComponent);
+				this.mergeComponents(sourceComponent, targetComponent);
 				for (N node : targetComponent) componentMap.put(node, sourceComponent);
 			}
 		}
 	}
 	
+	/**
+	 * Merges two components into a single component. More specifically, puts all elements of <code>otherComponent</code> into <code>component</code>.
+	 * @param component
+	 * @param otherComponent
+	 */
+	private void mergeComponents(Set<N> component, Set<N> otherComponent)
+	{
+		if (!this.components.contains(component)) throw new IllegalArgumentException("component must be part of the network");
+		if (!this.components.contains(otherComponent)) throw new IllegalArgumentException("otherComponent must be part of the network");
+		
+		if (component == otherComponent) throw new IllegalArgumentException("component and otherComponent must not be the same object");
+		
+		component.addAll(otherComponent);
+		this.components.remove(otherComponent);
+	}
+	
+	//This should probably return a copy, as a getting class could modify the component
 	public Set<N> getComponentOf(N node)
 	{
-		if (node == null) throw new IllegalArgumentException("node cannot be null");
-		if (!this.nodes.contains(node)) throw new IllegalArgumentException("node is not part of the network");
+		if (node == null) throw new IllegalArgumentException("node must not be null");
+		if (!this.nodes.contains(node)) throw new IllegalArgumentException("node must be part of the network");
 		
 		return this.components.stream().filter(c -> c.contains(node)).findAny().orElseThrow();
 	}
