9e6711fc5cc61540a32aafb7be27351e74764f85
The G-Meister <grantfutcher@live.co.uk>
2022-11-17 17:59:15 +0000

    Add getAllSourceEdgesOf in WebExploration

--- src/main/java/com/gmeister/temp/pkcmmsrando/network/WebExploration.java
+++ src/main/java/com/gmeister/temp/pkcmmsrando/network/WebExploration.java
@@ -1,5 +1,6 @@
 package com.gmeister.temp.pkcmmsrando.network;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WebExploration<N extends Node, E extends Edge<? extends N>> extends NetworkExploration<N, E>
@@ -8,14 +9,49 @@ public class WebExploration<N extends Node, E extends Edge<? extends N>> extends
 	public WebExploration()
 	{ super(); }
 	
+	public WebExploration(N startingNode)
+	{
+		super();
+		this.exploreFrom(startingNode);
+	}
+	
 	public WebExploration(List<N> nodesToExplore, List<N> nodesAccessed, List<E> edgesAccessed)
 	{ super(nodesToExplore, nodesAccessed, edgesAccessed); }
 	
 	public WebExploration(NetworkExploration<? extends N, ? extends E> other)
 	{ super(other); }
-
+	
 	public void exploreFrom(N node)
 	{ if (!this.nodesAccessed.contains(node)) this.nodesToExplore.add(node); }
+	
+	/**
+	 * Returns a list of all edges that can access a particular node in no particular order
+	 * @param target
+	 * @return
+	 */
+	public List<E> getAllSourceEdgesOf(N target)
+	{
+		if (!this.getNodesAccessed()
+				.contains(target))
+			throw new IllegalArgumentException("target must be part of the exploration");
+		
+		List<N> nodes = new ArrayList<>();
+		nodes.add(target);
+		List<E> paths = new ArrayList<>();
+		
+		for (int i = 0; i < nodes.size(); i++)
+		{
+			N node = nodes.get(i);
+			for (E edge : this.edgesAccessed) if (edge.getTarget()
+					.equals(node))
+			{
+				paths.add(edge);
+				if (!nodes.contains(edge.getSource())) nodes.add(edge.getSource());
+			}
+		}
+		
+		return paths;
+	}
 	
 	@Override
 	public void exploreEdge(E edge)
